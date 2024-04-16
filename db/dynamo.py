667e74b20c0cb71ae10cf77d0982bedb61ef0599
Igor Sena <72056234+igorsenam@users.noreply.github.com>
2024-04-16 10:28:26 -0300

    change: file structure

--- db/dynamo.py
+++ db/dynamo.py
@@ -3,7 +3,7 @@ import os
 
 dynamodb = resource("dynamodb",
                     aws_access_key_id=os.environ['AWS_ACCESS_KEY'],
-                    aws_secret_key_id=os.environ['AWS_SECRET_KEY'],
+                    aws_secret_access_key=os.environ['AWS_SECRET_KEY'],
                     region_name=os.environ['AWS_REGION_NAME'],
                     )
 
--- main.py
+++ main.py
@@ -1,3 +1,6 @@
 from fastapi import FastAPI
+from aws.dynamo import create_tables
 
 app = FastAPI()
+
+create_tables()
