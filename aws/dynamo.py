391b50b0e55c0a6f057a0b2188a773dbd8e52174
Igor Sena <72056234+igorsenam@users.noreply.github.com>
2024-04-16 10:20:24 -0300

    add: dynamo connection

---
+++ aws/dynamo.py
@@ -0,0 +1,48 @@
+from boto3 import resource
+import os
+
+dynamodb = resource("dynamodb",
+                    aws_access_key_id=os.environ['AWS_ACCESS_KEY'],
+                    aws_secret_key_id=os.environ['AWS_SECRET_KEY'],
+                    region_name=os.environ['AWS_REGION_NAME'],
+                    )
+
+tables = [
+    {
+        "TableName": "olly-database",
+        "KeySchema": [
+            {
+                'AttributeName': 'id',
+                'KeyType': 'HASH'
+            },
+            {
+                'AttributeName': 'created_at',
+                'KeyType': 'RANGE'
+            },
+
+        ],
+        "AttributeDefinitions": [
+            {
+                'AttributeName': 'id',
+                'AttributeType': 'S'
+            },
+            {
+                'AttributeName': 'created_at',
+                'AttributeType': 'S'
+            }
+        ],
+    },
+]
+
+
+def create_tables():
+    try:
+        for table in tables:
+            dynamodb.create_table(
+                TableName=table["TableName"],
+                KeySchema=table["KeySchema"],
+                AttributeDefinitions=table["AttributeDefinitions"],
+                BillingMode="PAY_PER_REQUEST"
+            )
+    except Exception as e:
+        print(e)
