9767c499921bd1a7672af40b34fbada43048ffa5
Igor Sena <72056234+igorsenam@users.noreply.github.com>
2024-04-17 12:27:01 -0300

    fix: JSONresponse added content

--- database/user.py
+++ database/user.py
@@ -7,6 +7,6 @@ table = dynamodb.Table("olly-database")
 def create_user(user: dict):
   try:
     table.put_item(Item=user)
-    return JSONResponse(status_code=200)
+    return JSONResponse(content={"response": "user created sucessfully"}, status_code=200)
   except ClientError as e:
     return JSONResponse(content=e.response["Error"], status_code=500)
